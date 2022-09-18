13d3926b801ec4bb0845783bfbd49132b7a823ee
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-09-17 21:07:41 -0700

    toggle nav hidden class on click

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+const burger = document.querySelector('#burger');
+
+if (burger){
+  burger.addEventListener('click', () => {
+    document.querySelector('#menu').classList.toggle('hidden');
+  });
+}
